76fb0bfa7378ceb9d7ee0a5fcba6d2c86cc13865
Rohan yadav <ryy3490@gmail.com>
2026-01-17 20:16:59 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 import sys
 
-def repl_cli():
+def read_cli():
     sys.stdout.write("$ ")
     prompt = input()
+    return prompt
+
+def echo_command(args):
+    print(" ".join(args))
+
+def repl_cli():
+    prompt = read_cli()
 
     if prompt == "exit":
         sys.exit()
+
+    command, args = prompt.split()[0], prompt.split()[1:]
+    match command:
+        case "echo":
+            echo_command(args)
+            return
+        case _:
+            pass
 
     print(f"{prompt}: command not found")
 
