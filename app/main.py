3dc607863da4544c782277309d72eebbb7a5e775
Rohan yadav <ryy3490@gmail.com>
2026-01-17 19:36:33 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     # sys.stdout.write("$ ")
     sys.stdout.write("$ ")
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
