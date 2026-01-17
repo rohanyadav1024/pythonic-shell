7a02a4f02e6763c80114de3ffd61382522fe3263
Rohan yadav <ryy3490@gmail.com>
2026-01-17 20:11:26 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 import sys
 
-def handle_cli():
+def repl_cli():
     sys.stdout.write("$ ")
     prompt = input()
+
+    if prompt == "exit":
+        sys.exit()
+
     print(f"{prompt}: command not found")
 
 def main():
     while True:
-        handle_cli()
+        repl_cli()
 
 if __name__ == "__main__":
     main()
